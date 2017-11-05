37f70ec463c9f8ac110a53bca69fca8a132ee454
Lorderot <Ihorkayf_@mail.ru>
2017-11-05 14:55:55 +0200

    Y prejction.... + validation

--- OSRCH.GUI/OSRCH.GUI/MainWindow.Designer.cs
+++ OSRCH.GUI/OSRCH.GUI/MainWindow.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pbProjectionX = new System.Windows.Forms.PictureBox();
+            this.pbProjectionY = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -39,26 +39,28 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProjectionX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProjectionY)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pbProjectionX
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(30, 43);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(301, 226);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pbProjectionX.Location = new System.Drawing.Point(30, 43);
+            this.pbProjectionX.Name = "pbProjectionX";
+            this.pbProjectionX.Size = new System.Drawing.Size(301, 226);
+            this.pbProjectionX.TabIndex = 0;
+            this.pbProjectionX.TabStop = false;
+            this.pbProjectionX.Paint += new System.Windows.Forms.PaintEventHandler(this.pbProjectionX_Paint);
             // 
-            // pictureBox2
+            // pbProjectionY
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(379, 43);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(301, 226);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pbProjectionY.Location = new System.Drawing.Point(379, 43);
+            this.pbProjectionY.Name = "pbProjectionY";
+            this.pbProjectionY.Size = new System.Drawing.Size(301, 226);
+            this.pbProjectionY.TabIndex = 1;
+            this.pbProjectionY.TabStop = false;
             // 
             // richTextBox1
             // 
@@ -144,22 +146,33 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Проекція Y";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(337, 264);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(990, 554);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pbProjectionY);
+            this.Controls.Add(this.pbProjectionX);
             this.Name = "MainWindow";
             this.Text = "ОСРЧ Лаба 1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProjectionX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProjectionY)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -169,8 +182,8 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pbProjectionX;
+        private System.Windows.Forms.PictureBox pbProjectionY;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
@@ -180,6 +193,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- OSRCH.GUI/OSRCH.GUI/MainWindow.cs
+++ OSRCH.GUI/OSRCH.GUI/MainWindow.cs
@@ -12,9 +12,144 @@ namespace OSRCH.GUI
 {
     public partial class MainWindow : Form
     {
+        #region Contants
+
+        //left margin of whole crane 
+        private const int BODY_MARGIN_LEFT = 30;
+        private const int BODY_MARGIN_TOP = 20;
+
+        //length of foot line
+        private const int FOOT_LENGTH = 20;
+        
+        private const int SHIPMENT_WIDTH = 20;
+        private const int SHIPMENT_HEIGHT = 30;
+
+        private const int PEN_WEIGHT = 2;
+        #endregion
+
+        #region Properties
+
+        protected int MoveLeftLength { get; set; }
+        protected int MoveRightLength { get; set; }
+        protected int MoveTopLength { get; set; }
+        protected int MoveBottomLength { get; set; }
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void pbProjectionX_Paint(object sender, PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            var bounds = e.ClipRectangle;
+
+            InitCrane(graphics, bounds);
+        }
+
+
+        private void ReDrawCrane()
+        {
+            pbProjectionX.Image = null;
+            InitCrane(pbProjectionX.CreateGraphics(), pbProjectionX.ClientRectangle);
+        }
+
+        private void InitCrane(Graphics graphics, Rectangle bounds)
+        {
+            DrawStaticElements(graphics, bounds);
+            DrawDynamicElements(graphics, bounds);
+        }
+
+        private void DrawStaticElements(Graphics graphics, Rectangle bounds)
+        {
+            using (var pen = new Pen(Color.Black, PEN_WEIGHT))
+            {
+                var relativeX = bounds.X + BODY_MARGIN_LEFT;
+                var relativeY = bounds.Y + BODY_MARGIN_TOP;
+
+                //draw foot
+                //(bounds.Height - 1) - to not crop line width.
+                graphics.DrawLine(pen, relativeX, bounds.Height - PEN_WEIGHT, relativeX + FOOT_LENGTH, bounds.Height - PEN_WEIGHT);
+
+                //draw body
+                graphics.DrawLine(pen, relativeX + FOOT_LENGTH / 2, bounds.Height, relativeX + FOOT_LENGTH / 2, relativeY);
+
+                //draw arrow (crane's dick)
+                graphics.DrawLine(pen, bounds.X, relativeY, bounds.Width, relativeY);
+
+            }
+        }
+
+        private void DrawDynamicElements(Graphics graphics, Rectangle bounds)
+        {
+            using (var pen = new Pen(Color.Black, PEN_WEIGHT))
+            {
+                var relativeX = bounds.X + BODY_MARGIN_LEFT + FOOT_LENGTH / 2 + SHIPMENT_WIDTH / 2 + PEN_WEIGHT;
+                var relativeY = bounds.Y + BODY_MARGIN_TOP;
+
+                //width where we can move dick to LEFT or RIGHT
+                var workingWidth = bounds.Width - relativeX ;
+                var halfOfWorkingWidth = workingWidth / 2;
+
+                //height where we can move dick to UP or DOWN
+                var workingHeight = bounds.Height - relativeY - SHIPMENT_HEIGHT / 2;
+                var halfOfWorkingHeight = workingHeight / 2;
+
+                //TODO: add or subtract, to move shipment LEFT or RIGHT
+                var positionX = halfOfWorkingWidth + MoveRightLength - MoveLeftLength ;
+
+                //TODO: add or subtract, to move shipment TOP or DOWN
+                var positionY = halfOfWorkingHeight + MoveBottomLength - MoveTopLength;
+
+                //TODO: add validation
+                //con't move to left or right more than halfOfWorkingWidth
+                //con't move to top or bottom more than halfOfWorkingHeight
+                if (!ValidateSemantic(workingWidth, workingHeight, positionX, positionY))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+
+                //draw shipment holder
+                graphics.DrawLine(pen, relativeX + positionX, relativeY, relativeX + positionX, relativeY + positionY);
+
+
+                //draw shipment
+                graphics.DrawRectangle(pen, relativeX + positionX - SHIPMENT_WIDTH/2, relativeY + positionY, SHIPMENT_WIDTH, SHIPMENT_HEIGHT);
+            }
+        }
+
+        private bool ValidateSemantic(int workingWidth, int workingHeight, int positionX, int positionY)
+        {
+            if (positionX <= 0 || positionX + SHIPMENT_WIDTH/2 >= workingWidth)
+            {
+                return false;
+            }
+
+            if (positionY <= 0 || positionY + SHIPMENT_HEIGHT / 2 >= workingHeight)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MoveBottomLength += 5;
+
+            try
+            {
+                ReDrawCrane();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MoveBottomLength -= 5;
+                ReDrawCrane();
+                MessageBox.Show("Impossible to move crane, because it's not in his working area!");
+            }
         }
     }
 }
