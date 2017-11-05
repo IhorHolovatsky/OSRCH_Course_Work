7c1ab08cc6c0d081d90771cd894892aace9e272d
Lorderot <Ihorkayf_@mail.ru>
2017-11-05 13:26:28 +0200

    changed design

--- OSRCH.GUI/OSRCH.GUI/MainWindow.Designer.cs
+++ OSRCH.GUI/OSRCH.GUI/MainWindow.Designer.cs
@@ -33,10 +33,12 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -44,7 +46,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(30, 21);
+            this.pictureBox1.Location = new System.Drawing.Point(30, 43);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(301, 226);
             this.pictureBox1.TabIndex = 0;
@@ -52,7 +54,7 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(379, 21);
+            this.pictureBox2.Location = new System.Drawing.Point(379, 43);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(301, 226);
             this.pictureBox2.TabIndex = 1;
@@ -60,7 +62,7 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(30, 272);
+            this.richTextBox1.Location = new System.Drawing.Point(30, 293);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(650, 235);
             this.richTextBox1.TabIndex = 2;
@@ -68,7 +70,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(706, 272);
+            this.groupBox1.Location = new System.Drawing.Point(706, 293);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(264, 235);
             this.groupBox1.TabIndex = 3;
@@ -81,39 +83,12 @@
             this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Location = new System.Drawing.Point(706, 21);
+            this.groupBox2.Location = new System.Drawing.Point(706, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(264, 226);
+            this.groupBox2.Size = new System.Drawing.Size(264, 257);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Дії";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(19, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 36);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Імпорт";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(19, 91);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(98, 36);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Старт";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(143, 91);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(98, 36);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Стоп";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -124,23 +99,71 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "text.txt";
             // 
-            // Form1
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Стоп";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Імпорт";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Проекція X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(379, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Проекція Y";
+            // 
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(990, 527);
+            this.ClientSize = new System.Drawing.Size(990, 554);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
+            this.Name = "MainWindow";
             this.Text = "ОСРЧ Лаба 1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -155,6 +178,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
